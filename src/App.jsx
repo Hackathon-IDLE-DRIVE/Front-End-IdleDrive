8434bfe1aed3fa5a94218093f0ae9b484d2d5fa7
DinnyS <118959844+DinnyS@users.noreply.github.com>
2023-10-28 21:56:46 +0700

    Feat : Add myCar component and add Mycar Page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 import ContentContainer from "./components/ContentContainer";
 import Login from "./pages/users/Login";
 import UserRegister from "./pages/register/userRegister";
+import { CarRentalList } from "./pages/car_rental/CarRentalList";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/tracking/:id" element={<Tracking />} />
           <Route path="/login" element={<Login/>}/>
           <Route path='/register/user' element={<UserRegister/>}/>
+          <Route path="/mycar" element={<CarRentalList/>}/>
         </Routes>
       </ContentContainer>
       <Footer/>
