336cf61f3505ebacbe6f7fbeb5cf77ebb774de31
DinnyS <118959844+DinnyS@users.noreply.github.com>
2023-11-03 14:19:56 +0700

    Fix : map button

--- src/components/Mapbutton.jsx
+++ src/components/Mapbutton.jsx
@@ -4,10 +4,10 @@ export default function Mapbutton() {
   return (
 
 
-    <a className='btn h-14 shadow-md bg-white'>
+    <a className='btn h-10 shadow-md bg-white'>
         <img className='h-10' 
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Google_Maps_icon_%282015-2020%29.svg/2048px-Google_Maps_icon_%282015-2020%29.svg.png' alt='map'
-        /><span className='pt-1.5 font-bold'>Open with Google Map</span></a>
+        src='https://miro.medium.com/v2/resize:fit:1024/0*oJK4t4_NTgdBPPgz.png' alt='map'
+        /><span className='pt-1 font-bold'>Open with Google Map</span></a>
 
   )
 }
